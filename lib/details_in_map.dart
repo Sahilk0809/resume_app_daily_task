96448759260fad4e8e768d58d911404880b3cc0b
Sahil Kashyap <149374235+Sahilk0809@users.noreply.github.com>
2024-05-06 15:46:29 +0530

    Update details_in_map.dart

--- lib/details_in_map.dart
+++ lib/details_in_map.dart
@@ -2,38 +2,22 @@ import 'dart:io';
 
 void main(){
   Map details = {
-    'name': 'Sahil',
-    'id': '1',
-    'address': 'Sai Darshan',
-    'contact': '5854845882',
-    'email': 'rkfg524@gmail.com',
-    'occupation': 'Service',
-    'gender': 'Male',
-    'state': 'Gujarat',
-    'city': 'Surat',
-    'pincode': '394210',
+    'name': Null,
+    'id': Null,
+    'address': Null,
+    'contact': Null,
+    'email': Null,
+    'occupation': Null,
+    'gender': Null,
+    'state': Null,
+    'city': Null,
+    'pincode': Null,
   };
 
-  stdout.write('Enter Name : ');
-  details.update('name', (value) => stdin.readLineSync()!);
-  stdout.write('Enter ID : ');
-  details.update('id', (value) => stdin.readLineSync()!);
-  stdout.write('Enter Address : ');
-  details.update('address', (value) => stdin.readLineSync()!);
-  stdout.write('Enter Contact : ');
-  details.update('contact', (value) => stdin.readLineSync()!);
-  stdout.write('Enter email : ');
-  details.update('email', (value) => stdin.readLineSync()!);
-  stdout.write('Enter occupation : ');
-  details.update('occupation', (value) => stdin.readLineSync()!);
-  stdout.write('Enter gender : ');
-  details.update('gender', (value) => stdin.readLineSync()!);
-  stdout.write('Enter state : ');
-  details.update('state', (value) => stdin.readLineSync()!);
-  stdout.write('Enter city : ');
-  details.update('city', (value) => stdin.readLineSync()!);
-  stdout.write('Enter pincode : ');
-  details.update('pincode', (value) => stdin.readLineSync()!);
+  for(String key in details.keys){
+    stdout.write('Enter $key : ');
+    details[key] = stdin.readLineSync();
+  }
 
   print('\n$details');
 }
