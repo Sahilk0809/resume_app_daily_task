ac0c318c942df55505660f01dcc33e2a38c25417
Sahil <rk4852115@gmail.com>
2024-05-08 16:00:30 +0530

    08-05-2024

---
+++ lib/routes/routes.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:resume_app_daily_task/Screens/gmail.dart';
+
+class MyRoutes{
+  static Map<String, Widget Function(BuildContext)> routes = {
+    '/': (context) => const Gmail(),
+  };
+}
---
+++ lib/utils/global.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+List detailsText = [
+  'All Inbox', 'Inbox', 'Starred', 'Snoozed', 'Important', 'Sent', 'Drafts', 'All Mail', 'Spam', 'Trash', 'Create New', 'Settings'
+];
+
+List detailsIcons = [
+  Icons.all_inbox, Icons.inbox, Icons.star_border, Icons.access_time, Icons.label_important_outline_sharp, Icons.send, Icons.drafts, Icons.forward_to_inbox, Icons.error_outline, Icons.delete_forever, Icons.add, Icons.settings
+];
