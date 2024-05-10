13cba4f9038e848737846dc078677c0bd52e1b31
Sahil <rk4852115@gmail.com>
2024-05-10 12:59:30 +0530

    10-05-2024

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:resume_app_daily_task/Screens/enter_screen/enterscreen.dart';
 import 'package:resume_app_daily_task/Screens/gmail/gmail.dart';
+import 'package:resume_app_daily_task/Screens/google/sign_in/email_screen.dart';
+import 'package:resume_app_daily_task/Screens/google/sign_in/password_screen.dart';
 import 'package:resume_app_daily_task/Screens/login_screen/loginScreen.dart';
 
 class MyRoutes{
   static Map<String, Widget Function(BuildContext)> routes = {
-    // '/gmail': (context) => const Gmail(),
-    '/': (context) => const EnterScreen(),
+    '/gmail': (context) => const Gmail(),
+    '/enter': (context) => const EnterScreen(),
     '/login': (context) => const LoginScreen(),
+    '/': (context) => const EmailScreen(),
+    '/password': (context) => const PasswordScreen(),
   };
 }
