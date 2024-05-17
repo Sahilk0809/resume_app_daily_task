05e3ea1242917f4544180d058b618b2fc2fa96ec
Sahil <rk4852115@gmail.com>
2024-05-17 16:07:00 +0530

    17-05-2024

---
+++ lib/Screens/generate_dynamic_text_field/dynamic_text_field.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DynamicTextField extends StatefulWidget {
+  const DynamicTextField({super.key});
+
+  @override
+  State<DynamicTextField> createState() => _DynamicTextFieldState();
+}
+
+class _DynamicTextFieldState extends State<DynamicTextField> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
